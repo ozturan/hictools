c808123e30c859825da53c41e97a484c0ff3ffc8
Dogancan Ozturan <dozturan@gmail.com>
2018-06-15 15:52:05 -0700

    first commit

---
+++ Juicebox2STM.py
@@ -0,0 +1,41 @@
+###########################################
+# Juicebox to sparse triangular matrices  #
+# for each chr-chr interactions			  #
+# Use various ozturan/hictools            #
+# for different Hi-C data formats.        #
+#										  #
+# Dogancan Ozturan               		  #
+# 2018									  #
+###########################################
+# usage: python Juicebox2STM.py <juicertools jar file> <input .hic file> <bin size> <fithic>
+
+
+from subprocess import call
+import sys
+
+# specify the juicertool name
+jarfile=sys.argv[1]
+# specify the .hic file name
+filename=sys.argv[2]
+# select the bin size
+bin=int(sys.argv[3])
+fithic=sys.argv[4]
+
+chromosomes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, "X", "Y"]
+
+def convert():
+	for i in chromosomes:
+	        call(["mkdir", "chr{0}".format(i)])
+	        for j in chromosomes:
+	                print "Dumping -> " "chr{0}".format(i), "chr{0}".format(j)
+	                call(["java", "-jar", "{0}".format(jarfile),
+	                        "dump", "observed", "NONE", "{0}".format(filename), "chr{0}".format(i), "chr{0}".format(j), "BP", "%d" %bin, "{0}_chr{1}_chr{2}.matrix".format(filename, i, j)])
+	                call(["mv", "{0}_chr{1}_chr{2}.matrix".format(filename, i, j), "chr{0}".format(i)])
+	                if fithic="fithic":
+	                	
+
+	                print "Done -> chr{0}_chr{1}.matrix".format(i, j)
+
+
+convert()
+print "\x1b[3;34;40m", "DUMPED! (Bin size:{0})".format(bin), "\x1b[0m"
---
+++ hic2FitHiC.R
@@ -0,0 +1,37 @@
+#!/usr/bin/env Rscript
+###########################################
+# Sparse triangular matrices to           #
+# Fit-Hi-C-ready interactions matrices		# 
+# Use various ozturan/hictools            #
+# for different Hi-C data formats.        #
+#										                      #
+# Dogancan Ozturan               		      #
+# 2018									                  #
+###########################################
+# usage: Rscript hic2FitHiC.R <filename> <chr1> <chr2> 
+library(readr)
+library(data.table)
+
+args = commandArgs(trailingOnly=TRUE)
+
+# data name
+dataname2=args[1]
+dataname=args[1]
+#select the chromosome
+chromosome1 = args[2]
+chromosome2 = args[3]
+
+dataname <- as.data.table(read_delim(sprintf("%s", dataname), 
+                                     "\t", escape_double = FALSE, col_names = FALSE, 
+                                     trim_ws = TRUE))
+
+
+dataname[, ':='  (chr1 = chromosome1, chr2 = chromosome2)]
+dataname <- dataname[,c(4, 1, 5, 2, 3)]
+
+# Save!
+write.table(dataname, file=sprintf('input_fithic_%s', dataname2), 
+            quote=FALSE, sep='\t', col.names = FALSE, row.names = FALSE)
+
+sprintf("Done!")
+
